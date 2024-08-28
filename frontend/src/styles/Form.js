ca3e11e5d883a98fe3ac9f2897e6616386ac1f20
NickelyMayara <nickelymayara28@gmail.com>
2024-08-28 18:27:28 -0300

    inicio de back e front

---
+++ frontend/src/styles/Form.js
@@ -0,0 +1,35 @@
+import styled from "styled-components"
+
+export const FormContainer = styled.form`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 10px;
+    flex-wrap: wrap;
+    box-shadow: 0px 0px 5px #ccc;
+    padding: 20px;
+    border-radius: 5px;
+    font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+    font-weight: 600;
+    font-size: 13px;
+`
+
+export const Label = styled.label`
+`
+
+export const Input = styled.input`
+    color: black;
+    padding: 5px;
+`
+
+export const Button = styled.button`
+    color: black;
+    cursor: pointer;
+    background-color: cornflowerblue;
+    padding: 8.5px;
+    border: transparent;
+    border-radius: 5px;
+    color: white;
+    font-weight: 600;
+    font-size: 11px;
+`
---
+++ frontend/src/styles/Table.js
@@ -0,0 +1,27 @@
+import styled from "styled-components"
+
+export const TableContainer = styled.table`
+    width: 100%;
+    padding: 20px;
+    box-shadow: 0px 0px 5px #ccc;
+    border-radius: 5px;
+    margin: 20px auto;
+    font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+    font-size: 14px;
+    word-break: break-all;
+`
+export const Thead = styled.thead`
+    
+`
+export const Tbody = styled.tbody`
+    
+`
+export const Tr = styled.tr`
+    
+`
+export const Th = styled.th`
+    font-weight: 600;
+`
+
+export const Td = styled.td`
+`
